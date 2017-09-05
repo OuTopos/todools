2e4afe03cd2dc9086f3ce062e78ebebc2a7121fc
Jonas Yamazaki <outopos@gmail.com>
2017-09-05 08:52:29 +0200

    Fixed incorrect selection on empty lines

--- decrease_priority.py
+++ decrease_priority.py
@@ -8,9 +8,14 @@ class DecreasePriorityCommand(sublime_plugin.TextCommand):
 		sel = self.view.sel()
 
 		for selection in sel:
+			keep_selection = True
+			if selection.a == selection.b:
+				keep_selection = False
 			line_offset = 0
 			lines = view.lines(selection)
 			for line in lines:
+				if line.a != line.b and not keep_selection:
+					keep_selection = True
 				adjusted_line = sublime.Region(line.a + line_offset, line.b + line_offset)
 				task = Task(view.substr(adjusted_line))
 				if not task.completed:
@@ -20,3 +25,6 @@ class DecreasePriorityCommand(sublime_plugin.TextCommand):
 						task.priority_letter = "A"
 					view.replace(edit, adjusted_line, task.string)
 					line_offset += task.offset
+			if not keep_selection:
+				sel.clear()
+				sel.add(sublime.Region(view.line(selection).b, view.line(selection).b))
